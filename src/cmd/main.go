b62c15565aa2d8230cfb566c8a168439a975e7c7
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-10-08 14:58:33 +0200

    Let the ElGamal signature implement it with pointer references

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,9 +1,13 @@
 package main
-import "sec.itu.dk/ex2/internals/signatures"
-import "fmt"
 
-func main()  {
+import (
+	"fmt"
+
+	"sec.itu.dk/ex2/internals/signatures"
+)
+
+func main() {
 	var signatureHandler signatures.SignatureHandler[int] = signatures.CreateNew()
-	var _, _ = signatureHandler.CreateSignature(2);
+	var _, _ = signatureHandler.CreateSignature(2)
 	fmt.Println("Hello World!")
 }
--- src/internals/signatures/ElGamal.go
+++ src/internals/signatures/ElGamal.go
@@ -5,22 +5,20 @@ type ElGamal struct {
 	publicKey    int
 }
 
-func CreateNew() ElGamal {
+func CreateNew() *ElGamal {
 	// TODO: Create random number in positive integers relative to a prime p-1
 	// TODO: Compute the public key
 
-	return ElGamal{signatureKey: 0, publicKey: 0}
+	return &ElGamal{signatureKey: 0, publicKey: 0}
 }
 
-// TODO: Change this to accept pointer recievers
-func (eg ElGamal) CreateSignature(message int) (r int, signature int) {
+func (eg *ElGamal) CreateSignature(message int) (r int, signature int) {
 	// TODO: Implement this function
 	return 0, 0
 }
 
 
-// TODO: Change this to accept pointer recievers
-func (eg ElGamal) VerifySignature(signature int, publicKey int) bool {
+func (eg *ElGamal) VerifySignature(signature int, publicKey int) bool {
 	// TODO: Implement the verification signature
 	return false
 }
