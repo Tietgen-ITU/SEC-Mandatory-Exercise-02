c75d31bf1a2589d741d10706fb3d84d4e8d16495
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-10-11 20:25:47 +0200

    Implement client without encryption and signature

--- src/cmd/client/main.go
+++ src/cmd/client/main.go
@@ -47,24 +47,44 @@ Is the main game loop for the client
 func playDiceGame(pk *big.Int,client *pb.DiceClient, ctx *context.Context) {
 
 	for i := 0; i < 6; i++ {
-		// 1. Generate dice roll for client
-		// 2. Generate dice roll for server
-		// 3. Who won?
+
+		if clientRoll, clientRollOk := diceRoll(pk, *client, ctx); clientRollOk {
+
+			if serverRoll, serverRollOk := diceRoll(pk, *client, ctx); serverRollOk {
+
+				if clientRoll > serverRoll {
+					fmt.Println("Client won!")
+				} else if clientRoll < serverRoll {
+					fmt.Println("Server won!")
+				} else {
+					fmt.Println("Draw...")
+				}
+			}
+		}
 	}
 }
 
+/*
+Makes the random dice roll in collaboration with the server
+*/
 func diceRoll(pk *big.Int, client pb.DiceClient, ctx *context.Context) (int, bool) {
 
+	// Generate commitment key and make dice roll
 	commitmentKey := rand.Int63()
 	roll := rand.Int31n(6)
 	for roll == 0 {
 		roll = rand.Int31n(6)
 	}
+	// TODO: Sign commitment message
 
+	// Send commitment of roll
 	commit := commitmentHandler.Commit(*big.NewInt(int64(roll)), *big.NewInt(commitmentKey))
-
 	serverCommitment, err := client.Commit(*ctx, &pb.Commitment{
 		Value: commit.Int64(),
+		Signature: &pb.Signature{
+			Signature: 0,
+			Random: 0,
+		},
 	})
 
 	if err != nil {
@@ -72,6 +92,11 @@ func diceRoll(pk *big.Int, client pb.DiceClient, ctx *context.Context) (int, boo
 		return 0, false
 	}
 
+	// TODO: Verify signature of server commitment message with pk
+
+	// TODO: Sign commitment reveal
+
+	// Reveal commitment to server
 	serverRollReveal, err := client.Reveal(*ctx, &pb.CommitmentReveal{
 		Value: roll,
 		Key: commitmentKey,
@@ -86,6 +111,9 @@ func diceRoll(pk *big.Int, client pb.DiceClient, ctx *context.Context) (int, boo
 		return 0, false
 	}
 
+	// TODO: Verify signature of server commitment reveal message with pk
+
+	// Calculate the random roll
 	serverValue := big.NewInt(int64(serverRollReveal.GetValue()))
 	serverCommitmentKey := big.NewInt(serverRollReveal.GetKey())
 	serverCommitmentValue := big.NewInt(serverCommitment.GetValue())
