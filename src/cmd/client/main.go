5c40bb024d1cb73ed1cd4e456455367cf0d1dd6b
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-10-11 20:07:23 +0200

    Implement dice roll function

--- src/cmd/client/main.go
+++ src/cmd/client/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"math/big"
+	"math/rand"
 
 	"google.golang.org/grpc"
 	pb "sec.itu.dk/ex2/api"
@@ -45,6 +46,55 @@ Is the main game loop for the client
 */
 func playDiceGame(pk *big.Int,client *pb.DiceClient, ctx *context.Context) {
 
+	for i := 0; i < 6; i++ {
+		// 1. Generate dice roll for client
+		// 2. Generate dice roll for server
+		// 3. Who won?
+	}
+}
+
+func diceRoll(pk *big.Int, client pb.DiceClient, ctx *context.Context) (int, bool) {
+
+	commitmentKey := rand.Int63()
+	roll := rand.Int31n(6)
+	for roll == 0 {
+		roll = rand.Int31n(6)
+	}
+
+	commit := commitmentHandler.Commit(*big.NewInt(int64(roll)), *big.NewInt(commitmentKey))
+
+	serverCommitment, err := client.Commit(*ctx, &pb.Commitment{
+		Value: commit.Int64(),
+	})
+
+	if err != nil {
+		fmt.Println("Could not send commitment to server!")
+		return 0, false
+	}
+
+	serverRollReveal, err := client.Reveal(*ctx, &pb.CommitmentReveal{
+		Value: roll,
+		Key: commitmentKey,
+		Signature: &pb.Signature{
+			Signature: 0,
+			Random: 0,
+		},
+	})
+
+	if err != nil {
+		fmt.Println("Could not reveal commitment to server!")
+		return 0, false
+	}
+
+	serverValue := big.NewInt(int64(serverRollReveal.GetValue()))
+	serverCommitmentKey := big.NewInt(serverRollReveal.GetKey())
+	serverCommitmentValue := big.NewInt(serverCommitment.GetValue())
+
+	correctMessage := commitmentHandler.Verify(*serverValue, *serverCommitmentValue, *serverCommitmentKey)
+
+	result := serverRollReveal.Value ^ roll
+
+	return int(result), correctMessage
 }
 
 /*
