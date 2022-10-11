fb7a3313dbfbba3c6b05fe787c9f8149319ffabe
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-10-11 16:18:35 +0200

    Change the commitment handler verify signature to include the commitment

--- src/internals/commitments/commitment_handler.go
+++ src/internals/commitments/commitment_handler.go
@@ -1,8 +1,12 @@
 package commitments
 
+import "math/big"
+
 
 type CommitmentHandler[MT any] interface {
 	// Takes the message and hashes it
-	Commit(MT) MT
-	Verify(MT, publicKey int) bool
+	Commit(message MT) MT
+
+	// Verifies that the message is equal to the commitment
+	Verify(message, commitment MT, publicKey big.Int) bool
 }
