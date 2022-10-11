d034034fd5904fc75470dc52eac31befb1a606b7
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-10-11 15:44:43 +0200

    Fix issues with key gen and signature validation

--- src/internals/signatures/signature_handler.go
+++ src/internals/signatures/signature_handler.go
@@ -3,6 +3,13 @@ package signatures
 import "math/big"
 
 type SignatureHandler[MT any] interface {
-	CreateSignature(MT) (random, s big.Int)
-	VerifySignature(message int, s, random, pk big.Int) bool
+
+	// Creates a signature based on the message
+	Sign(MT) (random, s big.Int)
+
+	// Verifies the signature
+	Verify(message int, s, random, pk big.Int) bool
+
+	// Gets the public key used for the signature handler
+	PublicKey() big.Int
 }
