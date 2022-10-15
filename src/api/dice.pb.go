e522754255b1148d4f572666a803ad07919e04ca
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-10-15 16:29:06 +0200

    Change proto to only contain dice service

--- src/api/dice.pb.go
+++ src/api/dice.pb.go
@@ -58,122 +58,18 @@ func (*Empty) Descriptor() ([]byte, []int) {
 	return file_dice_proto_rawDescGZIP(), []int{0}
 }
 
-type Key struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Value int64 `protobuf:"varint,1,opt,name=value,proto3" json:"value,omitempty"`
-}
-
-func (x *Key) Reset() {
-	*x = Key{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_dice_proto_msgTypes[1]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *Key) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*Key) ProtoMessage() {}
-
-func (x *Key) ProtoReflect() protoreflect.Message {
-	mi := &file_dice_proto_msgTypes[1]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use Key.ProtoReflect.Descriptor instead.
-func (*Key) Descriptor() ([]byte, []int) {
-	return file_dice_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *Key) GetValue() int64 {
-	if x != nil {
-		return x.Value
-	}
-	return 0
-}
-
-type Signature struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	// TODO: Check if we should change some of the types to use bytes(equivalent to []byte in Go) instead
-	Random    int64 `protobuf:"varint,1,opt,name=random,proto3" json:"random,omitempty"`       // Random key
-	Signature int64 `protobuf:"varint,2,opt,name=signature,proto3" json:"signature,omitempty"` // The actual signature
-}
-
-func (x *Signature) Reset() {
-	*x = Signature{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_dice_proto_msgTypes[2]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *Signature) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*Signature) ProtoMessage() {}
-
-func (x *Signature) ProtoReflect() protoreflect.Message {
-	mi := &file_dice_proto_msgTypes[2]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use Signature.ProtoReflect.Descriptor instead.
-func (*Signature) Descriptor() ([]byte, []int) {
-	return file_dice_proto_rawDescGZIP(), []int{2}
-}
-
-func (x *Signature) GetRandom() int64 {
-	if x != nil {
-		return x.Random
-	}
-	return 0
-}
-
-func (x *Signature) GetSignature() int64 {
-	if x != nil {
-		return x.Signature
-	}
-	return 0
-}
-
 type Commitment struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Value     int64      `protobuf:"varint,1,opt,name=value,proto3" json:"value,omitempty"`
-	Signature *Signature `protobuf:"bytes,2,opt,name=signature,proto3" json:"signature,omitempty"`
+	Value []byte `protobuf:"bytes,1,opt,name=value,proto3" json:"value,omitempty"`
 }
 
 func (x *Commitment) Reset() {
 	*x = Commitment{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_dice_proto_msgTypes[3]
+		mi := &file_dice_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -186,7 +82,7 @@ func (x *Commitment) String() string {
 func (*Commitment) ProtoMessage() {}
 
 func (x *Commitment) ProtoReflect() protoreflect.Message {
-	mi := &file_dice_proto_msgTypes[3]
+	mi := &file_dice_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -199,19 +95,12 @@ func (x *Commitment) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Commitment.ProtoReflect.Descriptor instead.
 func (*Commitment) Descriptor() ([]byte, []int) {
-	return file_dice_proto_rawDescGZIP(), []int{3}
+	return file_dice_proto_rawDescGZIP(), []int{1}
 }
 
-func (x *Commitment) GetValue() int64 {
+func (x *Commitment) GetValue() []byte {
 	if x != nil {
 		return x.Value
-	}
-	return 0
-}
-
-func (x *Commitment) GetSignature() *Signature {
-	if x != nil {
-		return x.Signature
 	}
 	return nil
 }
@@ -221,15 +110,14 @@ type CommitmentReveal struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Key       int64      `protobuf:"varint,1,opt,name=key,proto3" json:"key,omitempty"`
-	Value     int32      `protobuf:"varint,2,opt,name=value,proto3" json:"value,omitempty"`
-	Signature *Signature `protobuf:"bytes,3,opt,name=signature,proto3" json:"signature,omitempty"`
+	Key   []byte `protobuf:"bytes,1,opt,name=key,proto3" json:"key,omitempty"`
+	Value int32  `protobuf:"varint,2,opt,name=value,proto3" json:"value,omitempty"`
 }
 
 func (x *CommitmentReveal) Reset() {
 	*x = CommitmentReveal{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_dice_proto_msgTypes[4]
+		mi := &file_dice_proto_msgTypes[2]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -242,7 +130,7 @@ func (x *CommitmentReveal) String() string {
 func (*CommitmentReveal) ProtoMessage() {}
 
 func (x *CommitmentReveal) ProtoReflect() protoreflect.Message {
-	mi := &file_dice_proto_msgTypes[4]
+	mi := &file_dice_proto_msgTypes[2]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -255,14 +143,14 @@ func (x *CommitmentReveal) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use CommitmentReveal.ProtoReflect.Descriptor instead.
 func (*CommitmentReveal) Descriptor() ([]byte, []int) {
-	return file_dice_proto_rawDescGZIP(), []int{4}
+	return file_dice_proto_rawDescGZIP(), []int{2}
 }
 
-func (x *CommitmentReveal) GetKey() int64 {
+func (x *CommitmentReveal) GetKey() []byte {
 	if x != nil {
 		return x.Key
 	}
-	return 0
+	return nil
 }
 
 func (x *CommitmentReveal) GetValue() int32 {
@@ -272,45 +160,24 @@ func (x *CommitmentReveal) GetValue() int32 {
 	return 0
 }
 
-func (x *CommitmentReveal) GetSignature() *Signature {
-	if x != nil {
-		return x.Signature
-	}
-	return nil
-}
-
 var File_dice_proto protoreflect.FileDescriptor
 
 var file_dice_proto_rawDesc = []byte{
 	0x0a, 0x0a, 0x64, 0x69, 0x63, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x07, 0x0a, 0x05,
-	0x45, 0x6d, 0x70, 0x74, 0x79, 0x22, 0x1b, 0x0a, 0x03, 0x4b, 0x65, 0x79, 0x12, 0x14, 0x0a, 0x05,
-	0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x05, 0x76, 0x61, 0x6c,
-	0x75, 0x65, 0x22, 0x41, 0x0a, 0x09, 0x53, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x12,
-	0x16, 0x0a, 0x06, 0x72, 0x61, 0x6e, 0x64, 0x6f, 0x6d, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52,
-	0x06, 0x72, 0x61, 0x6e, 0x64, 0x6f, 0x6d, 0x12, 0x1c, 0x0a, 0x09, 0x73, 0x69, 0x67, 0x6e, 0x61,
-	0x74, 0x75, 0x72, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x03, 0x52, 0x09, 0x73, 0x69, 0x67, 0x6e,
-	0x61, 0x74, 0x75, 0x72, 0x65, 0x22, 0x4c, 0x0a, 0x0a, 0x43, 0x6f, 0x6d, 0x6d, 0x69, 0x74, 0x6d,
+	0x45, 0x6d, 0x70, 0x74, 0x79, 0x22, 0x22, 0x0a, 0x0a, 0x43, 0x6f, 0x6d, 0x6d, 0x69, 0x74, 0x6d,
 	0x65, 0x6e, 0x74, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x01, 0x20, 0x01,
-	0x28, 0x03, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x12, 0x28, 0x0a, 0x09, 0x73, 0x69, 0x67,
-	0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x0a, 0x2e, 0x53,
-	0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x52, 0x09, 0x73, 0x69, 0x67, 0x6e, 0x61, 0x74,
-	0x75, 0x72, 0x65, 0x22, 0x64, 0x0a, 0x10, 0x43, 0x6f, 0x6d, 0x6d, 0x69, 0x74, 0x6d, 0x65, 0x6e,
-	0x74, 0x52, 0x65, 0x76, 0x65, 0x61, 0x6c, 0x12, 0x10, 0x0a, 0x03, 0x6b, 0x65, 0x79, 0x18, 0x01,
-	0x20, 0x01, 0x28, 0x03, 0x52, 0x03, 0x6b, 0x65, 0x79, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c,
-	0x75, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x12,
-	0x28, 0x0a, 0x09, 0x73, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x18, 0x03, 0x20, 0x01,
-	0x28, 0x0b, 0x32, 0x0a, 0x2e, 0x53, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x52, 0x09,
-	0x73, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x32, 0x5a, 0x0a, 0x04, 0x44, 0x69, 0x63,
-	0x65, 0x12, 0x22, 0x0a, 0x06, 0x43, 0x6f, 0x6d, 0x6d, 0x69, 0x74, 0x12, 0x0b, 0x2e, 0x43, 0x6f,
-	0x6d, 0x6d, 0x69, 0x74, 0x6d, 0x65, 0x6e, 0x74, 0x1a, 0x0b, 0x2e, 0x43, 0x6f, 0x6d, 0x6d, 0x69,
-	0x74, 0x6d, 0x65, 0x6e, 0x74, 0x12, 0x2e, 0x0a, 0x06, 0x52, 0x65, 0x76, 0x65, 0x61, 0x6c, 0x12,
-	0x11, 0x2e, 0x43, 0x6f, 0x6d, 0x6d, 0x69, 0x74, 0x6d, 0x65, 0x6e, 0x74, 0x52, 0x65, 0x76, 0x65,
-	0x61, 0x6c, 0x1a, 0x11, 0x2e, 0x43, 0x6f, 0x6d, 0x6d, 0x69, 0x74, 0x6d, 0x65, 0x6e, 0x74, 0x52,
-	0x65, 0x76, 0x65, 0x61, 0x6c, 0x32, 0x27, 0x0a, 0x0b, 0x4b, 0x65, 0x79, 0x45, 0x78, 0x63, 0x68,
-	0x61, 0x6e, 0x67, 0x65, 0x12, 0x18, 0x0a, 0x0a, 0x45, 0x78, 0x63, 0x68, 0x61, 0x6e, 0x67, 0x65,
-	0x50, 0x6b, 0x12, 0x04, 0x2e, 0x4b, 0x65, 0x79, 0x1a, 0x04, 0x2e, 0x4b, 0x65, 0x79, 0x42, 0x10,
-	0x5a, 0x0e, 0x73, 0x65, 0x63, 0x2e, 0x69, 0x74, 0x75, 0x2e, 0x64, 0x6b, 0x2f, 0x61, 0x70, 0x69,
-	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x28, 0x0c, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x22, 0x3a, 0x0a, 0x10, 0x43, 0x6f, 0x6d,
+	0x6d, 0x69, 0x74, 0x6d, 0x65, 0x6e, 0x74, 0x52, 0x65, 0x76, 0x65, 0x61, 0x6c, 0x12, 0x10, 0x0a,
+	0x03, 0x6b, 0x65, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x03, 0x6b, 0x65, 0x79, 0x12,
+	0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05, 0x52, 0x05,
+	0x76, 0x61, 0x6c, 0x75, 0x65, 0x32, 0x5a, 0x0a, 0x04, 0x44, 0x69, 0x63, 0x65, 0x12, 0x22, 0x0a,
+	0x06, 0x43, 0x6f, 0x6d, 0x6d, 0x69, 0x74, 0x12, 0x0b, 0x2e, 0x43, 0x6f, 0x6d, 0x6d, 0x69, 0x74,
+	0x6d, 0x65, 0x6e, 0x74, 0x1a, 0x0b, 0x2e, 0x43, 0x6f, 0x6d, 0x6d, 0x69, 0x74, 0x6d, 0x65, 0x6e,
+	0x74, 0x12, 0x2e, 0x0a, 0x06, 0x52, 0x65, 0x76, 0x65, 0x61, 0x6c, 0x12, 0x11, 0x2e, 0x43, 0x6f,
+	0x6d, 0x6d, 0x69, 0x74, 0x6d, 0x65, 0x6e, 0x74, 0x52, 0x65, 0x76, 0x65, 0x61, 0x6c, 0x1a, 0x11,
+	0x2e, 0x43, 0x6f, 0x6d, 0x6d, 0x69, 0x74, 0x6d, 0x65, 0x6e, 0x74, 0x52, 0x65, 0x76, 0x65, 0x61,
+	0x6c, 0x42, 0x10, 0x5a, 0x0e, 0x73, 0x65, 0x63, 0x2e, 0x69, 0x74, 0x75, 0x2e, 0x64, 0x6b, 0x2f,
+	0x61, 0x70, 0x69, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -325,28 +192,22 @@ func file_dice_proto_rawDescGZIP() []byte {
 	return file_dice_proto_rawDescData
 }
 
-var file_dice_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
+var file_dice_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_dice_proto_goTypes = []interface{}{
 	(*Empty)(nil),            // 0: Empty
-	(*Key)(nil),              // 1: Key
-	(*Signature)(nil),        // 2: Signature
-	(*Commitment)(nil),       // 3: Commitment
-	(*CommitmentReveal)(nil), // 4: CommitmentReveal
+	(*Commitment)(nil),       // 1: Commitment
+	(*CommitmentReveal)(nil), // 2: CommitmentReveal
 }
 var file_dice_proto_depIdxs = []int32{
-	2, // 0: Commitment.signature:type_name -> Signature
-	2, // 1: CommitmentReveal.signature:type_name -> Signature
-	3, // 2: Dice.Commit:input_type -> Commitment
-	4, // 3: Dice.Reveal:input_type -> CommitmentReveal
-	1, // 4: KeyExchange.ExchangePk:input_type -> Key
-	3, // 5: Dice.Commit:output_type -> Commitment
-	4, // 6: Dice.Reveal:output_type -> CommitmentReveal
-	1, // 7: KeyExchange.ExchangePk:output_type -> Key
-	5, // [5:8] is the sub-list for method output_type
-	2, // [2:5] is the sub-list for method input_type
-	2, // [2:2] is the sub-list for extension type_name
-	2, // [2:2] is the sub-list for extension extendee
-	0, // [0:2] is the sub-list for field type_name
+	1, // 0: Dice.Commit:input_type -> Commitment
+	2, // 1: Dice.Reveal:input_type -> CommitmentReveal
+	1, // 2: Dice.Commit:output_type -> Commitment
+	2, // 3: Dice.Reveal:output_type -> CommitmentReveal
+	2, // [2:4] is the sub-list for method output_type
+	0, // [0:2] is the sub-list for method input_type
+	0, // [0:0] is the sub-list for extension type_name
+	0, // [0:0] is the sub-list for extension extendee
+	0, // [0:0] is the sub-list for field type_name
 }
 
 func init() { file_dice_proto_init() }
@@ -368,30 +229,6 @@ func file_dice_proto_init() {
 			}
 		}
 		file_dice_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*Key); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_dice_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*Signature); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_dice_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Commitment); i {
 			case 0:
 				return &v.state
@@ -403,7 +240,7 @@ func file_dice_proto_init() {
 				return nil
 			}
 		}
-		file_dice_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+		file_dice_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*CommitmentReveal); i {
 			case 0:
 				return &v.state
@@ -422,9 +259,9 @@ func file_dice_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_dice_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   5,
+			NumMessages:   3,
 			NumExtensions: 0,
-			NumServices:   2,
+			NumServices:   1,
 		},
 		GoTypes:           file_dice_proto_goTypes,
 		DependencyIndexes: file_dice_proto_depIdxs,
