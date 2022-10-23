b8b1e25d6e45c9c1a1386f3fb6a7dbc6acc79c02
Andreas Nicolaj Tietgen <9461145+Arneproductions@users.noreply.github.com>
2022-10-23 14:42:52 +0200

    Move dice roll to utils function

--- src/internals/utils/dice_roll_utils.go
+++ src/internals/utils/dice_roll_utils.go
@@ -1,12 +1,30 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type DiceRoll int
 type PartialRoll int
 
+var (
+	randomRollGenerator = rand.New(rand.NewSource(time.Now().UnixNano()))
+)
+
 func CalculateRoll(a, b PartialRoll) DiceRoll{
 	return DiceRoll(a ^ b)
+}
+
+func RollPartially() PartialRoll {
+	
+	roll := randomRollGenerator.Int31n(6)
+	for roll == 0 {
+		roll = randomRollGenerator.Int31n(6)
+	}
+
+	return PartialRoll(roll)
 }
 
 func PrintDiceRollWinner(client, server DiceRoll) {
